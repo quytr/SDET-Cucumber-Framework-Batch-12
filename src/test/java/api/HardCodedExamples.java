1ff51f3c744b47e5b07344bb36ee240e1fd29ec5
quytr <quy.truong310@gmail.com>
2022-06-15 19:13:43 -0400

    class 5 note

--- src/test/java/api/HardCodedExamples.java
+++ src/test/java/api/HardCodedExamples.java
@@ -6,11 +6,13 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class HardCodedExamples {
 
     String baseURI = RestAssured.baseURI = "http://hrm.syntaxtechs.net/syntaxapi/api";
     String token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTUyNDg1NTMsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY1NTI5MTc1MywidXNlcklkIjoiMzc4MCJ9.EpKOZZTJFApNeHFfVVWZymfz3xZfGeQNuBIENTR_ffc";
+    static String employee_id;
 
     @Test
     public void createEmployee() {
@@ -28,6 +30,16 @@ public class HardCodedExamples {
 
         Response response= request.when().post("/createEmployee.php");
         response.prettyPrint();
+
+        response.then().assertThat().statusCode(201);
+
+        //Hamcrest matchers
+        response.then().assertThat().body("Message", equalTo("Employee Created"));
+        response.then().assertThat().body("Employee.emp_firstname",equalTo("Chris"));
+
+        // using jsonPath(), to specify the key in the body so that it returns the value against it
+        employee_id = response.jsonPath().getString("Employee.employee_id");
+        System.out.println(employee_id);
 
 
 
